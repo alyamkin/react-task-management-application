bc8ae1dc212f3de5d453922062b37908538b42b6
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-06-29 19:42:46 -0400

    implement create task form

--- task-management-app/src/App.js
+++ task-management-app/src/App.js
@@ -1,7 +1,22 @@
+import { Fragment } from 'react';
 import Header from './components/Header';
+import CreateTaskForm from './components/CreateTaskForm';
+import { Container } from '@mui/material';
+import { Box } from '@mui/material';
+
+const tasks = [];
 
 function App() {
-  return <Header />;
+  const createTask = (title, description, dateTime) => {
+    console.log(title, description, dateTime);
+  };
+
+  return (
+    <Fragment>
+      <Header />
+      <CreateTaskForm onSubmitCreateTaskForm={createTask} />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ task-management-app/src/components/CreateTaskForm.js
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+const styles = {
+  field: {
+    marginBottom: '20px',
+  },
+  datePicker: {
+    width: '100%',
+    marginBottom: '20px',
+  },
+};
+
+export default function CreateTaskForm({ onSubmitCreateTaskForm }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [dueDateTime, setDueDateTime] = useState(dayjs());
+  const [titleError, setTitleError] = useState(false);
+  const [descriptionError, setDescriptionError] = useState(false);
+  const [dueDateTimeErrorMessage, setDueDateTimeErrorMessage] = useState('');
+
+  const isFormInvalid = () => {
+    const invalidTitle = title === '';
+    const invalidDescription = description === '';
+    const invalidDueDateTime = dayjs(dueDateTime) < dayjs();
+
+    if (invalidTitle) {
+      setTitleError(true);
+    }
+
+    if (invalidDescription) {
+      setDescriptionError(true);
+    }
+
+    if (invalidDueDateTime) {
+      setDueDateTimeErrorMessage('Please select future date and time');
+    }
+
+    return invalidTitle || invalidDescription || invalidDueDateTime;
+  };
+
+  const clearForm = () => {
+    setTitle('');
+    setDescription('');
+    setDueDateTime(dayjs());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormInvalid()) return;
+
+    onSubmitCreateTaskForm(title, description, dueDateTime);
+    clearForm();
+  };
+
+  const onChangeTitle = (e) => {
+    const value = e.target.value;
+    value.length > 0 ? setTitleError(false) : setTitleError(true);
+    setTitle(value);
+  };
+
+  const onChangeDescription = (e) => {
+    const value = e.target.value;
+    value.length > 0 ? setDescriptionError(false) : setDescriptionError(true);
+    setDescription(value);
+  };
+
+  const onChangeDateTimePicker = (value) => {
+    setDueDateTimeErrorMessage('');
+    setDueDateTime(dayjs(value));
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h6" component="h2" gutterBottom>
+        Create a new Task
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={onChangeTitle}
+          value={title}
+          sx={styles.field}
+          label="Title"
+          variant="outlined"
+          fullWidth
+          error={titleError}
+          required
+        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateTimePicker
+            sx={styles.datePicker}
+            label="Due Date and Time"
+            value={dueDateTime}
+            onChange={onChangeDateTimePicker}
+            disablePast
+            slotProps={{
+              textField: {
+                helperText: dueDateTimeErrorMessage,
+              },
+            }}
+          />
+        </LocalizationProvider>
+        <TextField
+          onChange={onChangeDescription}
+          value={description}
+          sx={styles.field}
+          label="Description"
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={4}
+          error={descriptionError}
+          required
+        />
+        <Button type="submit" color="primary" variant="contained">
+          {' '}
+          Create Task
+        </Button>
+      </form>
+    </Container>
+  );
+}
